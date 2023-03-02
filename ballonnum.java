bdf43b8d0d9441db745da01e24257daf5a8376e5
oovaa <omarvx211@gmail.com>
2023-03-02 17:30:18 +0200

    adding a custom snippet in omer.c file

--- ballonnum.java
+++ ballonnum.java
@@ -1,10 +1,11 @@
 public class ballonnum {
-    public static void main(String[] args) {
-     String text = "loonbalxballpoon";
-     System.out.println(maxNumberOfBalloons(text));
 
-    }
-    public static int maxNumberOfBalloons(String text) {
+  public static void main(String[] args) {
+    String text = "loonbalxballpoon";
+    System.out.println(maxNumberOfBalloons(text));
+  }
+
+  public static int maxNumberOfBalloons(String text) {
     //     int ans = 0;
     //    String tar = "balloon";
     //    StringBuilder hold = new StringBuilder();
@@ -13,29 +14,32 @@ public class ballonnum {
     //     if(tar.contains(ss)){
     //     hold.append(ss);
     //     }
-    //    } 
-       
+    //    }
+
     // System.out.println(hold);
-  
-       
-            int b = 0, a = 0, l = 0, o = 0, n = 0;
-            
-            for(char ch : text.toCharArray()){
-                switch(ch){
-                    case 'b': ++b;
-                            break;
-                    case 'a': ++a;
-                            break;
-                    case 'l': ++l;
-                            break;
-                    case 'o': ++o;
-                            break;
-                    case 'n': ++n;
-                            break;
-                
-                }
-            }
-            
-            return Math.min(Math.min(o/2, l/2), Math.min(Math.min(b, a), n));
-        }
+
+    int b = 0, a = 0, l = 0, o = 0, n = 0;
+
+    for (char ch : text.toCharArray()) {
+      switch (ch) {
+        case 'b':
+          ++b;
+          break;
+        case 'a':
+          ++a;
+          break;
+        case 'l':
+          ++l;
+          break;
+        case 'o':
+          ++o;
+          break;
+        case 'n':
+          ++n;
+          break;
+      }
     }
+
+    return Math.min(Math.min(o / 2, l / 2), Math.min(Math.min(b, a), n));
+  }
+}
