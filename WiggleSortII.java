c0b402866a16c76e4eef3ee9a7cf830eef9161f3
oovaa <omarvx211@gmail.com>
2023-12-11 20:19:50 +0200

    tranna do wiggle sort at 11-20

--- WiggleSortII.java
+++ WiggleSortII.java
@@ -1,23 +1,40 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class WiggleSortII {
 
   public static void main(String[] args) {
-    int[] nums = { 3, 5, 2, 1, 6, 4 };
+    int[] nums = { 1, 3, 7, 7, 2, 3, 1 };
 
     System.out.println("Original array: " + Arrays.toString(nums));
 
+    // wiggleSort(nums);
     wiggleSort(nums);
 
     System.out.println("Wiggle sorted array: " + Arrays.toString(nums));
   }
 
-  static void wiggleSort(int[] nums) {
-    // smalles latges
-    List<Integer> al = new ArrayList<Integer>(Arrays.asList(nums));
+  static void wiggleSort(int[] arr) {
+    int tmp, min, max, j, k;
+    for (int i = 0; i < arr.length - 1; i += 2) {
+      min = i;
+      max = i + 1;
 
-    System.err.println(al);
+      for (j = i + 1; j < arr.length; j++) if (arr[j] < arr[min]) min = j;
+      for (k = i + 2; k < arr.length; k++) if (arr[k] > arr[max]) max = k;
+
+      tmp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = tmp;
+
+      tmp = arr[i + 1];
+      arr[i + 1] = arr[max];
+      arr[max] = tmp;
+    }
   }
+  // static void wiggleSort(int[] nums) {
+  //   // smalles latges
+  //   List<Integer> al = new ArrayList<Integer>(Arrays.asList(nums));
+
+  //   System.err.println(al);
+  // }
 }
