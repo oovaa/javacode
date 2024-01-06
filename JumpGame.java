4fd31ec6f5e03f81b2390a45577565a97eba3c31
oovaa <omarvx211@gmail.com>
2024-01-06 10:32:25 +0200

    fix jumpGame at 06-10

--- JumpGame.java
+++ JumpGame.java
@@ -7,18 +7,17 @@ public class JumpGame {
   }
 
   static boolean canJump(int[] nums) {
-    int maxn;
-    int i;
+    int maxReach = 0;
+    int i = 0;
 
-    for (i = 0; i < nums.length - 1; i++) {
-      maxn = i + 1;
-      for (int j = i + 1; j < nums[i]; j++) if (nums[maxn] < nums[j]) maxn = j;
+    while (i < nums.length && i <= maxReach) {
+      maxReach = Math.max(maxReach, i + nums[i]);
+      i++;
 
-      if (nums[maxn] == 0) return false;
-
-      i = maxn - 1;
-      maxn = 0;
+      if (maxReach >= nums.length - 1)
+        return true;
     }
-    return (i == nums.length - 1) ? true : false;
+
+    return false;
   }
 }
